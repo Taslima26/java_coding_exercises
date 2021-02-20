b3e5c392c5e38f1e0fc07df5a7824e35f357d0e6
taslima26 <banglawalas@hotmail.co.uk>
2021-02-20 09:45:06 +0000

    refactored code after James feedback

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -5,36 +5,21 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
         return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
         return firstName.charAt(0) + "." + lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
         double answer = Math.round(originalPrice * (vatRate / 100 + 1) * 100.0) / 100.0;
         return answer;
     }
 
     public String checkSentenceReverse(String str) {
-        String words[] = str.split("\\s");
-        String reverseWord = "";
-        for (String w : words) {
-            StringBuilder sb = new StringBuilder(w);
-            sb.reverse();
-            reverseWord += sb.toString() + " ";
-        }
-        String reversedSentence = reverseWord.trim();
-        String reversedSentenceArray[] = reversedSentence.split(" ");
-        String ouputSentence = "";
-        for (int i = reversedSentenceArray.length - 1; i >= 0; i--) {
-            ouputSentence += reversedSentenceArray[i] + " ";
-        }
-        return ouputSentence.replaceAll("\\s+$", "");
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
 
     }
 
--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,20 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester( Person p1) {
-        if (p1.getCity() == "Manchester") return true;
-        else return false;
+    public boolean isFromManchester(Person p1) {
+        if (p1.getCity().equals("Manchester")) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public boolean canWatchFilm(Person p2, int ageLimit) {
-       if(ageLimit<=p2.getAge()) return true;
-       else return false;
+        if (ageLimit <= p2.getAge())
+            return true;
+        else
+            return false;
     }
-    
+
 }
